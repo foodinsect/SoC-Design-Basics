11d626c726e1655c2009d748112572dd1d2a311c
loongji <36304709+foodinsect@users.noreply.github.com>
2024-09-29 03:50:02 +0900

    upload

--- SimLab01/Practice4/ConstReg.v
+++ SimLab01/Practice4/ConstReg.v
@@ -11,27 +11,12 @@ module ConstReg #(
 (
     input           iRST,       // Reset input
     input           iCLK,       // Clock input
-<<<<<<< HEAD:SimLab01/Practice4/ConstReg.v
-    
-=======
-<<<<<<< HEAD
     
     input   [31:0]  iADR,       // Address input
     output  [31:0]  oDAT,       // Data output (read value)
     
     input           iSTB,       // Strobe signal (active high)
     input           iWE,        // Write Enable (Read or Write Signal)
-=======
->>>>>>> a2cf42e1c5fbff4827c7c7fc21bdf55b5343d746:SimLab01/SimLab01.srcs/sources_1/new/ConstReg.v
-    input   [31:0]  iADR,       // Address input
-    output  [31:0]  oDAT,       // Data output (read value)
-    
-    input           iSTB,       // Strobe signal (active high)
-<<<<<<< HEAD:SimLab01/Practice4/ConstReg.v
-    input           iWE,        // Write Enable (Read or Write Signal)
-=======
->>>>>>> 449bf3b4ec1b282ffaddda01f6845934e50168f2
->>>>>>> a2cf42e1c5fbff4827c7c7fc21bdf55b5343d746:SimLab01/SimLab01.srcs/sources_1/new/ConstReg.v
     output          oACK        // Acknowledge signal (S -> M)
 );
 
@@ -46,21 +31,9 @@ assign wSel3 = iSTB & (iADR == BaseAddrC); // 3rd const Address
 assign oACK = wSel1 | wSel2 | wSel3;
 
 // Data output based on address
-<<<<<<< HEAD:SimLab01/Practice4/ConstReg.v
 assign oDAT = (wSel1 & ~iWE) ? ID_VER1 :
               (wSel2 & ~iWE) ? ID_VER2 :
               (wSel3 & ~iWE) ? ID_VER3 :
-=======
-<<<<<<< HEAD
-assign oDAT = (wSel1 & ~iWE) ? ID_VER1 :
-              (wSel2 & ~iWE) ? ID_VER2 :
-              (wSel3 & ~iWE) ? ID_VER3 :
-=======
-assign oDAT = (wSel1) ? ID_VER1 :
-              (wSel2) ? ID_VER2 :
-              (wSel3) ? ID_VER3 :
->>>>>>> 449bf3b4ec1b282ffaddda01f6845934e50168f2
->>>>>>> a2cf42e1c5fbff4827c7c7fc21bdf55b5343d746:SimLab01/SimLab01.srcs/sources_1/new/ConstReg.v
               32'hzzzzzzzz;  // High impedance for invalid addresses
 
 endmodule
