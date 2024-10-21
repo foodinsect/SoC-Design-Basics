04c0162644921482c8b0d354b64a28dc5bf7f09e
loongji <36304709+foodinsect@users.noreply.github.com>
2024-10-22 00:55:04 +0900

    upload

---
+++ 04_ZynqLab/ip_repo/myFND_1.0/hdl/Hex2Dec.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2024/10/21 11:50:08
+// Design Name: 
+// Module Name: Hex2Dec
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Hex2Dec(
+
+    );
+endmodule
